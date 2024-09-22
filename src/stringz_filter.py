e37618f68489fa6c52718b70eac4572250bec358
Luca Russo <zwolfrost@gmail.com>
2024-09-22 12:00:52 +0200

    ottimizzazioni agli script

--- src/stringz_filter.py
+++ src/stringz_filter.py
@@ -1,7 +1,7 @@
 # Questo script permette di creare il file "stringz_filter.txt", partendo dal file "stringz_full.txt" (tutte le stringhe di gioco).
 # Il file "stringz_filter.txt" conterrà solo le stringhe in lingua spagnola, utili per smistare le stringhe da tradurre velocemente.
-import json, re
 from lingua import Language, LanguageDetectorBuilder
+from utils import get_stringz_full, get_translation, clean_dialog
 
 
 # Apri i file
@@ -10,25 +10,11 @@ STRINGZ_FULL_FILEPATH = "./stringz_full.txt"
 STRINGZ_FILTER_FILEPATH = "./stringz_filter.txt"
 
 try:
-	with open(TRANSLATION_FILEPATH, 'r', encoding='utf-8') as f:
-		translation = json.loads(f.read())
-	with open(STRINGZ_FULL_FILEPATH, 'r', encoding='utf-8', errors='ignore') as f:
-		stringz_full = f.read().split('\n')
+	translation = get_translation(TRANSLATION_FILEPATH)[0]
+	stringz_offsets, stringz_lines = get_stringz_full(STRINGZ_FULL_FILEPATH)
 except FileNotFoundError as e:
 	print(f"File '{e.filename}' non trovato.")
 	exit(1)
-
-
-# Dividi i dialoghi in stringz
-stringz_offsets = []
-stringz_lines = []
-for line in stringz_full:
-	if line.startswith('_' * 80):
-		stringz_offsets.append(line)
-	elif len(stringz_lines) < len(stringz_offsets):
-		stringz_lines.append(line)
-	else:
-		stringz_lines[-1] += '\n' + line
 
 
 # Setup del classificatore di lingua
@@ -47,13 +33,11 @@ with open(STRINGZ_FILTER_FILEPATH, 'a', encoding='utf-8') as stringz_filter:
 	stringz_filter.truncate(0)
 	stringz_filter.write('_' * 80 + "https://github.com/zWolfrost/Katana-ZERO-Traduzione-Italiana")
 
-	stringz_index = 0
-	for line in stringz_lines:
-		if line not in translation and all(ch in CHAR_WHITELIST for ch in line):
-			line_clean = re.sub(r'\[.*?\]|\*', '', line)
-			confidence = detector.compute_language_confidence_values(line_clean)[0]
+	for stringz_index in range(len(stringz_lines)):
+		line = stringz_lines[stringz_index]
+		if line and line not in translation and all(ch in CHAR_WHITELIST for ch in line):
+			confidence = detector.compute_language_confidence_values(clean_dialog(line))[0]
 			if confidence.language == Language.SPANISH and confidence.value > 0.5:
-				stringz_filter.write(f"\n{stringz_offsets[stringz_index]}_{confidence.value:.2f}\n{line}")
+				stringz_filter.write(f"\n{stringz_offsets[stringz_index]}___{confidence.value:.2f}\n{line}")
 
-		stringz_index += 1
 		print(round(stringz_index / len(stringz_lines) * 100), end='%\r')
--- src/stringz_patch.py
+++ src/stringz_patch.py
@@ -1,6 +1,6 @@
 # Questo script permette di creare il file "stringz_patch.txt",
 # partendo dal file "stringz_full.txt" (tutte le stringhe di gioco) e dal file "translation.json" (le traduzioni delle stringhe).
-import json
+from utils import get_stringz_full, get_translation, replace_with_table
 
 
 # Apri i file
@@ -9,65 +9,41 @@ STRINGZ_FULL_FILEPATH = "./stringz_full.txt"
 STRINGZ_PATCH_FILEPATH = "../stringz_patch.txt"
 
 try:
-	with open(TRANSLATION_FILEPATH, 'r', encoding='utf-8') as f:
-		translation: dict[str, str] = json.loads(f.read())
-	with open(STRINGZ_FULL_FILEPATH, 'r', encoding='utf-8', errors='ignore') as f:
-		stringz_full = f.read().split('\n')
+	translation, translation_settings = get_translation(TRANSLATION_FILEPATH)
+	stringz_offsets, stringz_lines = get_stringz_full(STRINGZ_FULL_FILEPATH)
 except FileNotFoundError as e:
 	print(f"File '{e.filename}' non trovato.")
 	exit(1)
 
 
-# Dividi i dialoghi in stringz
-stringz_offsets = []
-stringz_lines = []
-for line in stringz_full:
-	if line.startswith('_' * 80):
-		stringz_offsets.append(line)
-	elif len(stringz_lines) < len(stringz_offsets):
-		stringz_lines.append(line)
-	else:
-		stringz_lines[-1] += '\n' + line
-
-
 # Controlla se la traduzione contiene tutte le linee di dialogo una sola volta
-translation_counter = {t: 0 for t in dict(translation)}
+translation_counter = {l: 0 for l in translation}
 for line in stringz_lines:
 	if line in translation:
 		translation_counter[line] += 1
 for line, count in translation_counter.items():
-	if line.startswith('###'):
-		continue
 	if count == 0:
 		print(f"La seguente linea è inesistente:\n{line}\n")
 		del translation[line]
-	elif count != 1:
+	elif count >= 2:
 		print(f"La seguente linea è presente più di una volta:\n{line}\n")
 
 
 # Inserisci le stringhe in stringz-patch.txt
-def replace_with_table(string, table):
-	for key, value in table.items():
-		string = string.replace(key, value)
-	return string
-
 with open(STRINGZ_PATCH_FILEPATH, 'a', encoding='utf-8') as stringz_patch:
 	stringz_patch.truncate(0)
 	stringz_patch.write('_' * 80 + "https://github.com/zWolfrost/Katana-ZERO-Traduzione-Italiana")
 
 	stringz_index = 0
 	for line in translation:
-		if line.startswith('###'):
-			continue
-
 		try:
 			stringz_index = stringz_lines.index(line, stringz_index + 1)
 		except ValueError:
 			print(f"La seguente linea è fuori ordine:\n{line}\n")
 			continue
 
-		offsets = replace_with_table(stringz_offsets[stringz_index], translation["###"]["offset_replace"])
-		line_translated = replace_with_table(translation[line], translation["###"]["string_replace"])
+		offsets = replace_with_table(stringz_offsets[stringz_index], translation_settings["offset_replace"])
+		line_translated = replace_with_table(translation[line], translation_settings["string_replace"])
 
 		stringz_patch.write('\n' + offsets + '\n' + line_translated)
 
