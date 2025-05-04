b1a8cdabac1f924308b879afddf5a9f46cc6eb38
Luca Russo <zwolfrost@gmail.com>
2025-05-04 19:11:43 +0200

    aggiungi tasto di rimozione

--- kz_patcher.py
+++ kz_patcher.py
@@ -12,9 +12,15 @@
 # Dipendenze: strindex, pyxdelta, pyside6 (installabili tramite pip)
 
 import os, hashlib, urllib.request, pyxdelta
-from PySide6.QtWidgets import QMessageBox
+from PySide6 import QtWidgets
 import strindex.strindex as strindex
 from strindex.gui import StrindexGUI
+from strindex.utils import PrintProgress
+
+POSSIBLE_LOCATIONS = [
+	"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Katana ZERO\\Katana ZERO.exe",
+	"/home/zwolfrost/.steam/steam/steamapps/common/Katana ZERO/Katana ZERO.exe"
+]
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/zWolfrost/Katana-ZERO-Traduzione-Italiana"
 KZ_PATCH_URL = DOWNLOAD_ROOT + "/main/kz_patch.gz"
@@ -32,20 +38,24 @@ def get_file_md5(file):
 
 def download_file(url):
 	try:
-		filepath, _ = urllib.request.urlretrieve(url)
+		return urllib.request.urlretrieve(url)[0]
 	except Exception as e:
 		raise ConnectionError(f'Errore durante il download del file all\'url "{url}": {e}')
-	return filepath
 
 class KatanaZeroPatchGUI(StrindexGUI):
+	def start_action(self):
+		for widget in self.findChildren(QtWidgets.QWidget):
+			if isinstance(widget, QtWidgets.QProgressBar):
+				PrintProgress.callback = lambda progress: widget.setValue(progress.percent)
+
 	def patch(self, katanazero_filepath):
 		# Controlla l'esistenza dei file di gioco
-		if not os.path.exists(katanazero_filepath) or os.path.basename(katanazero_filepath) != "Katana ZERO.exe":
+		if not os.path.isfile(katanazero_filepath) or os.path.basename(katanazero_filepath) != "Katana ZERO.exe":
 			raise FileNotFoundError('File "Katana ZERO.exe" errato o non trovato.')
 
 		datawin_filepath = os.path.join(os.path.dirname(katanazero_filepath), "data.win")
 
-		if not os.path.exists(datawin_filepath):
+		if not os.path.isfile(datawin_filepath):
 			raise FileNotFoundError('File "data.win" non trovato.')
 
 		# Questo controllo non è per niente affidabile, infatti non bloccherà il processo.
@@ -54,7 +64,7 @@ class KatanaZeroPatchGUI(StrindexGUI):
 			self.show_message((
 				"Questo gioco è stato probabilmente piratato. "
 				"La patch avverrà malgrado questo ma per favore considera di supportare gli sviluppatori ;)"
-			), QMessageBox.Warning)
+			), QtWidgets.QMessageBox.Warning)
 
 		# Patcha Katana ZERO.exe
 		strindex_filepath = download_file(KZ_PATCH_URL)
@@ -64,7 +74,7 @@ class KatanaZeroPatchGUI(StrindexGUI):
 
 		# Rileva il tipo di data.win, e scarica il file xdelta corretto
 		datawin_bak_filepath = datawin_filepath + ".bak"
-		datawin_md5 = get_file_md5(datawin_bak_filepath if os.path.exists(datawin_bak_filepath) else datawin_filepath)
+		datawin_md5 = get_file_md5(datawin_bak_filepath if os.path.isfile(datawin_bak_filepath) else datawin_filepath)
 
 		if datawin_md5 in XDELTA_URL_BY_MD5:
 			datawin_xdelta_filepath = download_file(XDELTA_URL_BY_MD5[datawin_md5])
@@ -73,32 +83,74 @@ class KatanaZeroPatchGUI(StrindexGUI):
 				"File data.win non valido. "
 				"La traduzione è stata patchata ma alcune lettere potrebbero avere accenti sbagliati. "
 				"Assicurati di avere la versione più recente del gioco."
-			), QMessageBox.Warning)
+			), QtWidgets.QMessageBox.Warning)
 			return
 
-		if not os.path.exists(datawin_bak_filepath):
+		if not os.path.isfile(datawin_bak_filepath):
 			os.rename(datawin_filepath, datawin_bak_filepath)
 
 		# Patcha data.win
 		pyxdelta.decode(datawin_bak_filepath, datawin_xdelta_filepath, datawin_filepath)
 
+	def remove(self, katanazero_filepath):
+		# Ripristina i file di gioco dai backup
+		katanazero_bak_filepath = katanazero_filepath + ".bak"
+
+		if not os.path.isfile(katanazero_bak_filepath) or os.path.basename(katanazero_bak_filepath) != "Katana ZERO.exe.bak":
+			raise FileNotFoundError((
+				'File di backup "Katana ZERO.exe.bak" non trovato. '
+				'Non è possibile ripristinare il gioco (o è già allo stato originale).'
+			))
+
+		if os.path.isfile(katanazero_filepath):
+			os.remove(katanazero_filepath)
+
+		os.rename(katanazero_bak_filepath, katanazero_filepath)
+
+		datawin_filepath = os.path.join(os.path.dirname(katanazero_filepath), "data.win")
+		datawin_bak_filepath = datawin_filepath + ".bak"
+
+		if os.path.isfile(datawin_filepath):
+			os.remove(datawin_filepath)
+
+		os.rename(datawin_bak_filepath, datawin_filepath)
+
+	def update_buttons(self, katanazero_filepath):
+		enabled = os.path.isfile(katanazero_filepath)
+		self.__widgets__[-1].setEnabled(enabled)
+		self.__widgets__[-2].setEnabled(enabled)
+
 	def setup(self):
-		self.create_file_selection(
+		line_edit = self.create_file_selection(
 			line_text="*Seleziona il file eseguibile di Katana ZERO",
 			button_text="Seleziona file"
-		)
+		)[0]
+
+		line_edit.textChanged.connect(self.update_buttons)
 
 		self.create_action_button(
 			text="Esegui Patch",
 			progress_text="Patch in corso... %p%",
 			complete_text="Patch avvenuta con successo.",
-			callback=self.patch,
+			callback=(self.start_action(), self.patch()),
 		)
-		self.create_padding(1)
+
+		self.create_action_button(
+			text="Rimuovi Patch",
+			progress_text="Rimozione... %p%",
+			complete_text="Rimozione patch avvenuta con successo.",
+			callback=(self.start_action(), self.remove()),
+		)
 
 		self.create_grid_layout(2).setColumnStretch(0, 1)
 
 		self.set_window_properties(title="Katana ZERO - Traduzione Italiana")
+
+		for path in POSSIBLE_LOCATIONS:
+			if os.path.isfile(path):
+				line_edit.setText(path)
+				self.update_buttons(path)
+				break
 
 if __name__ == "__main__":
 	KatanaZeroPatchGUI()
