4be8cedbc4701913a2a972db5ce6f4e16ad8a27a
Luca Russo <zwolfrost@gmail.com>
2026-07-15 14:37:56 +0200

    aggiornato alla versione di strindex piu recente

--- patcher/patcher.py
+++ patcher/patcher.py
@@ -18,18 +18,19 @@ import urllib.error
 import ssl
 import hashlib
 import pyxdelta
+import strindex.filetypes.pe, strindex.utils, strindex.gui
 from PySide6 import QtWidgets, QtCore, QtGui
-from strindex import strindex
-from strindex.gui import MainStrindexGUI
-from strindex.utils import PrintWrapper, PrintProgress
 
 # Disabilita i messaggi della libreria strindex.
-PrintWrapper.QUIET = True
+strindex.utils.PrintWrapper.QUIET = True
 
 # Formato degli URL dei file di patch.
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/zWolfrost/Katana-ZERO-Traduzione-Italiana/main/patches/"
 KZ_EXE_STRINDEX_URL = DOWNLOAD_ROOT + "kz_exe.gz"
 DATAWIN_XDELTA_URL_FMT = DOWNLOAD_ROOT + "datawin_{id}.xdelta"
+
+# Costante per la lunghezza dell'ID md5 da usare nei nomi dei file di backup.
+MD5_SLICE = 8
 
 # Crea un worker per i segnali di avviso tra thread diversi.
 class SignalWorker(QtCore.QObject):
@@ -39,7 +40,6 @@ signals = SignalWorker()
 def get_file_md5_id(file: str) -> str:
 	""" Restituisci i primi 8 caratteri dell'ID md5 del file. """
 
-	MD5_SLICE = 8
 	with open(file, "rb") as f:
 		file_hash = hashlib.md5()
 		while chunk := f.read(262144):
@@ -110,7 +110,7 @@ def check_game_files(katanazero_filepath: str) -> tuple[str, str]:
 def remove_and_patch(katanazero_filepath: str, datawin_filepath: str):
 	""" Rimuovi la patch (se già presente) e poi (ri)applicala. """
 
-	print_progress = PrintProgress(8)
+	print_progress = strindex.utils.PrintProgress(8)
 
 	# Rimuovi la patch precedente (se esiste)
 	try:
@@ -132,11 +132,19 @@ def remove_and_patch(katanazero_filepath: str, datawin_filepath: str):
 				"Assicurati di avere la versione più recente di questo programma."
 			)
 		raise
+
 	print_progress(2)
 
 	# Patcha Katana ZERO.exe
 	try:
-		strindex.patch(katanazero_filepath, katanazero_strindex_filepath, None)
+		data = strindex.filetypes.pe.patch(
+			strindex.utils.FileBytearray.read(katanazero_filepath),
+			strindex.utils.Strindex.read(katanazero_strindex_filepath)
+		)
+
+		os.replace(katanazero_filepath, katanazero_filepath + "_" + data.md5[:MD5_SLICE] + ".bak")
+
+		data.write(katanazero_filepath)
 	except ValueError as e:
 		if ".strdex" in str(e):
 			raise Exception(
@@ -145,10 +153,14 @@ def remove_and_patch(katanazero_filepath: str, datawin_filepath: str):
 				"o reinstalla il gioco da capo."
 			)
 		raise
+
+	print("Il file \"Katana ZERO.exe\" è stato patchato con successo.")
+
 	print_progress(3)
 
 	# Rileva l'ID md5 di data.win
 	datawin_xdelta_id = get_file_md5_id(datawin_filepath)
+
 	print_progress(4)
 
 	# Scarica il file xdelta giusto per data.win
@@ -163,20 +175,24 @@ def remove_and_patch(katanazero_filepath: str, datawin_filepath: str):
 			)
 			return
 		raise
+
 	print_progress(5)
 
 	# Crea un backup di data.win
 	datawin_bak_filepath = datawin_filepath + ".bak"
 	os.replace(datawin_filepath, datawin_bak_filepath)
+
 	print_progress(6)
 
 	# Patcha data.win
 	pyxdelta.decode(datawin_bak_filepath, datawin_xdelta_filepath, datawin_filepath)
 	print("Il file \"data.win\" è stato patchato con successo.")
+
 	print_progress(7)
 
 	# Rinomina il backup di data.win
 	os.replace(datawin_bak_filepath, get_file_bak_filepath(datawin_filepath))
+
 	print_progress(8)
 
 def remove(*game_files: str):
@@ -206,7 +222,7 @@ def remove(*game_files: str):
 			"Se hai già rimosso la patch, ignora questo messaggio."
 		)
 
-class KatanaZeroPatchGUI(MainStrindexGUI):
+class KatanaZeroPatchGUI(strindex.gui.MainStrindexGUI):
 	def setup(self):
 		SELF_LOCATION = os.path.abspath(os.path.dirname(__file__))
 
